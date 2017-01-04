0f5ac5ed15cef01d2783863b8f8b30b79bc63d61
Alexey Komissarouk <alexey@alexeymk.com>
2017-01-04 15:02:41 -0500

    commit random things that weren't making it.

--- publish.sh
+++ publish.sh
@@ -7,12 +7,15 @@ PUBLISH_BRANCH=master
 set -ex
 
 # ensure the content in /_site is up to date
-
-rm -r $PUBLISH_DIR/* || echo "Couldn't remove"
+cd $PUBLISH_DIR
+git checkout $PUBLISH_BRANCH
+rm -r * || echo "Couldn't remove"
+cd -
 jekyll build --destination $PUBLISH_DIR
 
 # commit everything
 cd $PUBLISH_DIR
+git checkout $PUBLISH_BRANCH
 git add .
 git commit -am "$*"
 git push
