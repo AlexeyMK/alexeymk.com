25376110007d3b674742a0982e72f25c62e6b3fe
Alexey Komissarouk <alexey@alexeymk.com>
2020-05-13 02:13:47 -0700

    wip (fixing publish script)

--- publish.sh
+++ publish.sh
@@ -2,19 +2,24 @@
 # IDEA: statically publish blog by pushing an updated _site to the master branch.
 # assume: ../publish_alexeymk.com (or similar directory) has the same git repo
 
-PUBLISH_DIR=./_site
-PUBLISH_BRANCH=master
+PUBLISH_REPO=git@github.com:AlexeyMK/alexeymk.github.com.git
+TMP_LOC=/tmp/site
+
 set -ex
+if [[ $# -eq 0 ]] ; then
+  echo 'Usage: `bash publish.sh Name of the change`. '
+  echo 'No commit title, no commit.'
+  exit 0
+fi
 
 # ensure the content in /_site is up to date
-cd $PUBLISH_DIR
-git checkout $PUBLISH_BRANCH
-rm -r * || echo "Couldn't remove"
-cd -
-jekyll build --destination $PUBLISH_DIR
+rm -r $TMP_LOC || echo "Couldn't remove"
+mkdir $TMP_LOC
+git clone $PUBLISH_REPO $TMP_LOC
+jekyll build --destination $TMP_LOC
 
 # commit everything
-cd $PUBLISH_DIR
+cd $TMP_LOC
 git add .
 git commit -am "$*"
 git push
